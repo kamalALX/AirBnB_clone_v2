355799e8f1e6609ad077d3c885a371d43988a9cf
kamal <kamalmiftah01@gmail.com>
2024-04-16 22:05:13 +0100

    mm

--- models/state.py
+++ models/state.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.city import City
+from models.base_model import Base
+from sqlalchemy import Column, String, relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete-orphan", backref="state")
+
+    @property
+    def cities(self):
+        lista = [city for city in BaseModel.__objects.values() if
+            isinstance(city, City) and city.state_id == State.id]
+        return lista

59af878d489b37d1061f7ad33966ccac5b74307b
kamal <kamalmiftah01@gmail.com>
2024-04-17 00:44:48 +0100

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+import models
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
 
 Base = declarative_base()
@@ -18,17 +18,21 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -58,4 +62,4 @@ class BaseModel:
 
     def delete(self):
         """ deletes the current instance from the storage """
-        storage.delete(self)
+        models.storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import models
 from models.city import City
-from models.base_model import Base
 from sqlalchemy import Column, String, relationship
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete-orphan", backref="state")
+    cities = relationship("City", cascade="all, delete, delete-orphan", backref="state")
 
     @property
     def cities(self):
-        lista = [city for city in BaseModel.__objects.values() if
+        lista = [city for city in models.storage.all() if
             isinstance(city, City) and city.state_id == State.id]
         return lista

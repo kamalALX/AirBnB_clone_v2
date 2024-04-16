f5e4db86de78a2a0d617b0149d755a94ef69c80a
Joseph Eiba <josepheiba@icloud.com>
2024-04-16 21:13:23 +0100

    added Base = declarative_base() and ID

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = sqlalchemy.Column(sqlalchemy.String(60), nullable=False, primary_key=True)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:

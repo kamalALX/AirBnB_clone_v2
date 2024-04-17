5db21eaa214bba76229a6d8caca983427713b545
Joseph Eiba <josepheiba@icloud.com>
2024-04-17 15:37:28 +0100

    updating dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,6 +5,13 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
 from models.base_model import BaseModel
 
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 class DBStorage():
     __engine = None
     __session = None

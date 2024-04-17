43d9b95cef0768faf8f5812380e63ca2b311a330
kamal <kamalmiftah01@gmail.com>
2024-04-17 03:00:43 +0100

    fix

--- console.py
+++ console.py
@@ -216,11 +216,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.__objects.items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.__objects.items():
                 print_list.append(str(v))
 
         print(print_list)
@@ -233,7 +233,7 @@ class HBNBCommand(cmd.Cmd):
     def do_count(self, args):
         """Count current number of class instances"""
         count = 0
-        for k, v in storage._FileStorage__objects.items():
+        for k, v in storage.__objects.items():
             if args == k.split('.')[0]:
                 count += 1
         print(count)

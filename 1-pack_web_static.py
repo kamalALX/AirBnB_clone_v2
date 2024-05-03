d322dda1f2d6deed8ef07a20a1d63848a657a211
Kamal <kamalmiftah01@gmail.com>
2024-05-03 15:36:51 +0100

    fixing task 0

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,13 +10,13 @@ def do_pack():
     """ making aechive if web_static folder"""
 
     local("mkdir -p versions")
-    
+
     time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = "web_static_{}.tgz".format(time_stamp)
     
     command = "tar -cvzf versions/{} web_static".format(archive_name)
     result = local(command)
-    
+
     if result.failed:
         return None
     return archive

4b551b7c565fd5172281eb6d4728ccdbf8863ed3
Kamal <kamalmiftah01@gmail.com>
2024-05-03 04:02:42 +0100

    solving task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,6 +22,14 @@ sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+echo "
+server {
+	listen 80 default_server;
+
+	location /hbnb_static {
+		alias /data/web_static/current/;
+	}
+}
+" | sudo tee /etc/nginx/sites-enabled/default
 
 sudo service nginx restart

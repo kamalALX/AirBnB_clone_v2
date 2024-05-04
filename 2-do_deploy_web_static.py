3051e42193d266d069bfb5ce7595fc8b39067e96
Kamal <kamalmiftah01@gmail.com>
2024-05-03 17:06:22 -0700

    solving task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,23 +9,16 @@ env.hosts = ['100.25.165.190', '54.162.91.137']
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if not exists(archive_path):
+    if exists(archive_path) is False:
         return False
-
-
     try:
         archive_filename = archive_path.split('/')[-1]
         archive_name = archive_filename.split('.')[0]
         path = "/data/web_static/releases/"
-
         put(archive_path, '/tmp/')
-
-        run("mkdir -p /data/web_static/releases/{}/".format(archive_name))
-
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_filename, archive_name))
-
+        run("mkdir -p {} {}/".format(path, archive_name))
+        run("tar -xzf /tmp/{} -C {}{}/".format(archive_filename, path, archive_name))
         run("rm /tmp/{}".format(archive_filename))
-
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, archive_name))
         run('rm -rf {}{}/web_static'.format(path, archive_name))
         run('rm -rf /data/web_static/current')

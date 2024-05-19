d4e67132266cec12da5d114f15347d2e795532f2
Kamal <kamalmiftah01@gmail.com>
2024-05-19 23:38:39 +0100

    solving task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,16 +9,9 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """HTML page with a list of all"""
     states = storage.all(State).values()
     return render_template("8-cities_by_states.html", states=states)
 
@@ -27,7 +20,6 @@ def states_list():
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
